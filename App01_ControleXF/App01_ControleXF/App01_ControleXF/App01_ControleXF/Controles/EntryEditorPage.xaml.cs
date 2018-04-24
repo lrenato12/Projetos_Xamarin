70d7d9b3c827066a8d9f13a8e207eba91ec21435
luiz.renato <renatim9100@gmail.com>
2018-04-24 13:59:54 -0300

    App sobre os controles Xamarin Forms.

---
+++ App01_ControleXF/App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/EntryEditorPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EntryEditorPage : ContentPage
+	{
+		public EntryEditorPage ()
+		{
+			InitializeComponent ();
+
+            txtIdade.TextChanged += delegate(object sender, TextChangedEventArgs args)
+            {
+                lblDuplicado.Text = args.NewTextValue;
+            };
+
+            txtComentario.Completed += delegate(object sender, EventArgs args)
+            {
+                lblQuantidadeCaracteres.Text = txtComentario.Text.Length.ToString();
+            };
+        }
+	}
+}
---
+++ App01_ControleXF/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoActivityIndicatorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ActivityIndicatorPage();
+        }
+
+        private void GoProgressBarPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ProgressBarPage();
+        }
+
+        private void GoBoxViewPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.BoxViewPage();
+        }
+
+        private void GoLabelPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.LabelPage();
+        }
+
+        private void GoButtonPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ButtonPage();
+        }
+
+        private void GoEntryEditorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.EntryEditorPage();
+        }
+    }
+}
