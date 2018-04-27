ea6080044ec8c1bf7988735aaa9dcf443ef8f966
luiz.renato <renatim9100@gmail.com>
2018-04-27 13:58:42 -0300

    App de Tarefas.

---
+++ App2_Tarefa/App2_Tarefa/App2_Tarefa/App2_Tarefa/Modelos/GerenciadorTarefa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_Tarefa.Modelos
+{
+    public class GerenciadorTarefa
+    {
+        private List<Tarefa> Lista { get; set; }
+
+        public void Salvar(Tarefa tarefa)
+        {
+            Lista.Add(tarefa);
+            SalvarNoProperties(Lista);
+        }
+        public void Deletar(Tarefa tarefa)
+        {
+            Lista.Remove(tarefa);
+            SalvarNoProperties(Lista);
+        }
+        public void Finalizar(int index, Tarefa tarefa)
+        {
+            Lista.RemoveAt(index);
+            Lista.Add(tarefa);
+            SalvarNoProperties(Lista);
+        }
+        public List<Tarefa> Listar()
+        {
+            return ListarNoProperties();
+        }
+        private List<Tarefa> ListarNoProperties()
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+                return (List<Tarefa>)App.Current.Properties["Tarefas"];
+            else
+                return new List<Tarefa>();
+        }
+        private void SalvarNoProperties(List<Tarefa> Lista)
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+                App.Current.Properties.Remove("Tarefas");
+
+            App.Current.Properties.Add("Tarefas", Lista);
+        }
+    }
+}
---
+++ App2_Tarefa/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Inicio.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_Tarefa.Telas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Inicio : ContentPage
+	{
+		public Inicio ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ActionGoCadastro(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Cadastro());
+        }
+    }
+}
