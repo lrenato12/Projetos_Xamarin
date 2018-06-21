d011278fa54d5004d0ed4f945454cfbf348bd1eb
luiz.renato <renatim9100@gmail.com>
2018-06-21 13:32:00 -0300

    Curso Xamarin

---
+++ App2_ListaBrasil/App2_ListaBrasil/App2_ListaBrasil/App2_ListaBrasil/Estados.xaml.cs
@@ -0,0 +1,28 @@
+﻿using App2_ListaBrasil.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_ListaBrasil
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Estados : ContentPage
+	{
+		public Estados ()
+		{
+			InitializeComponent ();
+            ListaEstados.ItemsSource = Servico.Servico.GetEstados();
+		}
+
+        private void SelecaoEstadoAction(object sender, SelectedItemChangedEventArgs args)
+        {
+            Estado estado = (Estado)args.SelectedItem;
+            Navigation.PushAsync(new Municipios(estado));
+        }
+    }
+}
---
+++ App2_ListaBrasil/App2_ListaBrasil/App2_ListaBrasil/App2_ListaBrasil/Municipios.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App2_ListaBrasil.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_ListaBrasil
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Municipios : ContentPage
+	{
+        private List<Municipio> ListaInternaMunicipio { get; set; }
+        private List<Municipio> ListaFiltradaMunicipio { get; set; }
+
+        public Municipios (Estado estado)
+		{
+			InitializeComponent ();
+            ListaInternaMunicipio = Servico.Servico.GetMunicipio(estado.id);
+            ListaMunicipio.ItemsSource = ListaInternaMunicipio;
+        }
+
+        private void BuscaRapida(object sender, TextChangedEventArgs args)
+        {
+            ListaFiltradaMunicipio = ListaInternaMunicipio.Where(a => a.nome.Contains(args.NewTextValue)).ToList();
+            ListaMunicipio.ItemsSource = ListaFiltradaMunicipio;
+        }
+    }
+}
