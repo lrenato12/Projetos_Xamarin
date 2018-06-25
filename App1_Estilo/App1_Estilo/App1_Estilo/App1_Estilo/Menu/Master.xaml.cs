888082d2ff6f126e455a3359d284fff533f35f83
luiz.renato <renatim9100@gmail.com>
2018-06-25 13:35:35 -0300

    Curso Xamarin

---
+++ App1_Estilo/App1_Estilo/App1_Estilo/App1_Estilo/Menu/Master.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Estilo.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+    {
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoPagina1(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.ImplicityStylePage());
+        }
+
+        private void GoPagina2(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.ExplicityStylePage());
+        }
+
+        private void GoPagina3(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.GlobalStylePage());
+        }
+
+        private void GoPagina4(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.InheritPage());
+        }
+
+        private void GoPagina5(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Paginas.DynamicStylePage());
+        }
+    }
+}
